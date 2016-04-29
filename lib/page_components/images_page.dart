b8827d47871430c75df19a3c7156233040d52196
cdnielson <cdnielson@folkprophet.com>
2016-04-29 06:40:55 -0600

    typos and layout issues fixed

--- lib/page_components/images_page.dart
+++ lib/page_components/images_page.dart
@@ -8,10 +8,16 @@ class ImagesPage {
   SiteService siteService;
   static const String PATH_TO_RILLA_IMAGES = "images/rogg/images/";
   List images = [
-    {"src":"${PATH_TO_RILLA_IMAGES}image1.jpg","width":"400px","height":""},
-    {"src":"${PATH_TO_RILLA_IMAGES}image3.jpg","width":"","height":"400px"},
-    {"src":"${PATH_TO_RILLA_IMAGES}image2.jpg","width":"400px","height":""},
-    {"src":"${PATH_TO_RILLA_IMAGES}image4.jpg","width":"","height":"400px"},
+    {"src":"${PATH_TO_RILLA_IMAGES}image1.jpg","width":"350px","height":""},
+    {"src":"${PATH_TO_RILLA_IMAGES}image3.jpg","width":"","height":"350px"},
+    {"src":"${PATH_TO_RILLA_IMAGES}image2.jpg","width":"350px","height":""},
+    {"src":"${PATH_TO_RILLA_IMAGES}image4.jpg","width":"","height":"350px"},
+    {"src":"${PATH_TO_RILLA_IMAGES}ROGG-116.jpg","width":"350px","height":""},
+    {"src":"${PATH_TO_RILLA_IMAGES}ROGG-121.jpg","width":"350px","height":""},
+    {"src":"${PATH_TO_RILLA_IMAGES}ROGG-170.jpg","width":"350px","height":""},
+    {"src":"${PATH_TO_RILLA_IMAGES}ROGG-278.jpg","width":"350px","height":""},
+    {"src":"${PATH_TO_RILLA_IMAGES}ROGG-452.jpg","width":"","height":"350px"},
+    {"src":"${PATH_TO_RILLA_IMAGES}ROGG-487.jpg","width":"","height":"350px"}
   ];
 
   ImagesPage(SiteService this.siteService);
